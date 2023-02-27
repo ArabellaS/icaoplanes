16714ff948fa7d97dc118b53c50e96c3d831d64e
Cecile Saiz <cecile.saiz@gmail.com>
2023-02-27 15:06:26 +0100

    Plane model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_131356) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_135616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planes", force: :cascade do |t|
+    t.string "name"
+    t.integer "seats"
+    t.integer "speed"
+    t.string "location"
+    t.integer "category"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_planes_on_category"
+    t.index ["user_id"], name: "index_planes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_131356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "planes", "users"
 end
