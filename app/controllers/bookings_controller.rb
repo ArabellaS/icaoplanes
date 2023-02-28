f16dd2e94b01e25a53a1e90012a93f6720ea3aa4
Cecile Saiz <cecile.saiz@gmail.com>
2023-02-28 17:42:57 +0100

    Authorizations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,16 @@
 class BookingsController < ApplicationController
+
+  before_action :set_booking, except: [:new, :create]
+
   def new
     @booking = Booking.new
+    authorize @booking
     @plane = Plane.find(params[:plane_id])
   end
 
   def create
     @booking = Booking.new(booking_params)
+    authorize @booking
     @booking.plane = Plane.find(params[:plane_id])
     @booking.user = current_user
     if @booking.save
@@ -16,16 +21,30 @@ class BookingsController < ApplicationController
   end
 
   def validate
-    @booking = Booking.find(params[:id])
+    authorize @booking
     @booking.update(status: :accepted)
   end
 
   def deny
-    @booking = Booking.find(params[:id])
+    authorize @booking
     @booking.update(status: :denied)
   end
 
+  def update
+    authorize @booking
+    @booking.update
+  end
+
+  def destroy
+    authorize @booking
+    @booking.destroy
+  end
+
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -1,18 +1,22 @@
 class PlanesController < ApplicationController
+
+  before_action :set_plane, except: [:index, :new, :create]
   def index
-    @planes = Plane.all
+    @planes = policy_scope(Plane)
   end
 
   def show
-    @plane = Plane.find(params[:id])
+    authorize @plane
   end
 
   def new
     @plane = Plane.new
+    authorize @plane
   end
 
   def create
     @plane = Plane.new(plane_params)
+    authorize @plane
     @plane.user = current_user
     if @plane.save
       redirect_to plane_path(@plane)
@@ -21,7 +25,24 @@ class PlanesController < ApplicationController
     end
   end
 
+  def edit
+    authorize @plane
+  end
+
+  def update
+    authorize @plane
+    @plane.update
+  end
+
+  def destroy
+    authorize @plane
+  end
+
   private
+
+  def set_plane
+    @plane = Plane.find(params[:id])
+  end
 
   def plane_params
     params.require(:plane).permit(:name, :seats, :speed, :location, :category, :photo)
