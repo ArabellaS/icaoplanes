92fe3d749955817543e7f1c89ca974949f20ba57
Alexis Queune <alexis.queune1@gmail.com>
2023-02-28 16:09:08 +0100

    created profile page, set up enum, add links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  # à retravailler une fois qu'on aura fait les autorisations
+  def profile
+    @bookings = Booking.all
+    @planes = Plane.all
+  end
 end
