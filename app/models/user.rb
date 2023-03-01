dbd15adf68ad9806640f4a92a49e7f3d02b38e7f
Cecile Saiz <cecile.saiz@gmail.com>
2023-03-01 14:36:36 +0100

    Repaired sign-up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_many :planes
 
-  validates :name, presence: true
-  validates :dob, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
