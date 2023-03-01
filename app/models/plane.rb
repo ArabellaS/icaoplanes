80cb3263bb5fa8b0b649dbd1b98b14a3b915cb62
Alexis Queune <alexis.queune1@gmail.com>
2023-03-01 16:02:02 +0100

    add map to planes page

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -3,6 +3,9 @@ class Plane < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   enum category: {
     small: 1,
     business: 2,
