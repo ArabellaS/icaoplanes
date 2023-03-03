86758db04f61cb9750c5e19468a8f90ef5278f95
Alexis Queune <alexis.queune1@gmail.com>
2023-03-03 12:44:10 +0100

    add price to profil page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def diff_in_days(start_date, end_date)
+    if start_date && end_date
+      diff = (end_date.to_date - start_date.to_date).to_i
+      # diff += 1 # add 1 day by default
+    else
+      diff = 0
+    end
+    diff
+  end
 end
